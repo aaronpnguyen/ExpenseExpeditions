806620138c3ac659e783d566672b41e7ce4fd13e
Amiculay <aaronnguyen1123@gmail.com>
2022-11-16 02:55:57 -0500

    Charts are now available for expeditions

--- client/src/components/Expedition.js
+++ client/src/components/Expedition.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import ExpenseForm from './ExpenseForm'
 import ExpenseList from './ExpenseList'
+import Statistic from './Statistic'
 
 const Expedition = () => {
     const [submit, setSubmit] = useState(false)
@@ -11,7 +12,7 @@ const Expedition = () => {
         <>
             <ExpenseForm expeditionId={id} submit={submit} setSubmit={setSubmit}/>
             <ExpenseList expeditionId={id} submit={submit}/>
-            <p>{id}</p>
+            <Statistic expeditionId={id} submit={submit}/>
         </>
     )
 }
--- client/src/components/Statistic.js
+++ client/src/components/Statistic.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from 'react-router-dom'
 import {DataParser, ChartInfo, Sum} from "./assets/functions"
 import axios from 'axios'
 import {Doughnut, Bar, Pie} from 'react-chartjs-2';
@@ -7,9 +8,12 @@ import {Chart as ChartJS} from 'chart.js/auto'
 const Chart = submit => {
     const [expense, setExpense] = useState();
     const [sum, setSum] = useState(0);
+    const {id} = useParams()
 
     useEffect(() => {
-        axios.get("http://localhost:8000/api/finances/user", {withCredentials: true})
+        let route = ""
+        id? route = `http://localhost:8000/api/finances/expedition/${id}`: route = "http://localhost:8000/api/finances/user"
+        axios.get(route, {withCredentials: true})
             .then(response => {
                 let data = DataParser(response)
                 setExpense(ChartInfo(data))
